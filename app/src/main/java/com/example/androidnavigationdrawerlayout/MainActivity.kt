5343ddaafc43d71dcc751c0e595c22e4470b8c9b
Ratiug-99 <evgeniy.basiuk@gmail.com>
2021-02-16 22:31:48 +0200

    fix bug when on NO_home_fragment always was enabled backPressed button

--- app/src/main/java/com/example/androidnavigationdrawerlayout/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigationdrawerlayout/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +20,19 @@ class MainActivity : AppCompatActivity() {
 
         navController = Navigation.findNavController(this, R.id.fragment)
 
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.fragmentOne, R.id.fragmentTwo, R.id.fragmentFree), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navigationView.setupWithNavController(navController)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
-        NavigationUI.setupWithNavController(navigationView, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }

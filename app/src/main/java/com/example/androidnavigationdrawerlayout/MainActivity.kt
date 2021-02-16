3baa8927db96a9355fb8ced37f93839b3c38d369
Ratiug-99 <evgeniy.basiuk@gmail.com>
2021-02-16 22:18:59 +0200

    create working menu

--- app/src/main/java/com/example/androidnavigationdrawerlayout/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigationdrawerlayout/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.androidnavigationdrawerlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolBar)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
+    }
+
 }
